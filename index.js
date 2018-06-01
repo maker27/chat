9ef2448e57c26bf15fd12ee0c89365cb17191f49
maker27 <support930@mail.ru>
2018-06-01 23:00:38 +0300

    Main

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const app = require('express')(),
+const express = require('express'),
+    app = express(),
     http = require('http').Server(app),
     io = require('socket.io')(http);
 
+app.use(express.static('static'));
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
