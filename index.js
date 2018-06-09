f36a0507d48e19b9210b2d85c945e322043526d5
maker27 <support930@mail.ru>
2018-06-09 14:27:09 +0300

    Main

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express'),
     app = express(),
     http = require('http').Server(app),
-    io = require('socket.io')(http);
+    io = require('socket.io')(http),
+
+    allClients = {},
+    flags = ['ad','bj','cu','ga','ie','lc','mu','pl','sm','ua','ae','bm','cv','gb','il','li','mv','pm','sn','ug','af','bn','cx','gd','im','lk','mw','pn','so','uk','ag','bo','cy','ge','in','lr','mx','pr','sr','um','ai','br','cz','gf','io','ls','my','ps','ss','us','al','bs','de','gh','iq','lt','mz','pt','st','uy','am','bt','dj','gi','ir','lu','na','pw','sv','uz','an','bv','dk','gl','is','lv','nc','py','sy','va','ao','bw','dm','gm','it','ly','ne','qa','sz','vc','aq','by','do','gn','jm','ma','nf','re','tc','ve','ar','bz','dz','gp','jo','mc','ng','ro','td','vg','as','ca','ec','gq','jp','md','ni','rs','tf','vi','at','cc','ee','gr','ke','me','nl','ru','tg','vn','au','cd','eg','gs','kg','mg','no','rw','th','vu','aw','cf','eh','gt','kh','mh','np','sa','tj','wf','ax','cg','er','gu','ki','mk','nr','sb','tk','ws','az','ch','es','gw','km','ml','nu','sc','tl','ye','ba','ci','et','gy','kn','mm','nz','sd','tm','yt','bb','ck','eu','hk','kp','mn','om','se','tn','za','bd','cl','fi','hm','kr','mo','pa','sg','to','zm','be','cm','fj','hn','kw','mp','pe','sh','tr','zw','bf','cn','fk','hr','ky','mq','pf','si','tt','bg','co','fm','ht','kz','mr','pg','sj','tv','bh','cr','fo','hu','la','ms','ph','sk','tw','bi','cs','fr','id','lb','mt','pk','sl','tz'];
 
 app.use(express.static('static'));
 
@@ -9,14 +12,30 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-    socket.broadcast.emit('connection');
+function getClientsCount(){
+    return Object.keys(io.sockets.sockets).length;
+}
+function getRandomPid(){
+    var randEl, flagsCount = flags.length;
+    do{
+        randEl = flags[Math.floor(Math.random()*flagsCount)];
+    } while(allClients[randEl] && flagsCount>Object.keys(allClients).length);
+    return randEl;
+}
+
+io.on('connection', function(socket,callback){
+    socket.number = getRandomPid();
+    allClients[socket.number] = Date.now();
+    socket.broadcast.emit('connection',socket.number);
+    io.emit('count',getClientsCount());
 
     socket.on('disconnect', function(){
-        socket.broadcast.emit('disconnection');
+        socket.broadcast.emit('disconnection',socket.number);
+        delete allClients[socket.number];
+        io.emit('count',getClientsCount());
     });
     socket.on('message',function(data){
-        if(data) io.emit('message',data);
+        if(data) io.emit('message',{text:data,pid:socket.number});
     });
 });
 
--- static/script.js
+++ static/script.js
@@ -2,9 +2,15 @@ const socket = io(),
     chat = document.getElementById('chat'),
     input = document.getElementById('msgText'),
 
-    insertString = function(text){
+    insertString = function(text,classname,icon){
         let newText = document.createElement('div');
+        if(classname) newText.className = classname;
         newText.textContent = text;
+        if(icon){
+            let iconSpan = document.createElement('span');
+            iconSpan.style.background = 'url(http://maffia-online.ru/images/flags/'+icon+'.png) no-repeat';
+            newText.appendChild(iconSpan);
+        }
         chat.appendChild(newText);
         chat.scrollTop = chat.scrollHeight;
     },
@@ -12,6 +18,19 @@ const socket = io(),
         if(!input.value) return;
         socket.emit('message', input.value);
         input.value = '';
+    },
+    someThing = function(count,first,some,many){
+        var out, tmp = count%100;
+        if(tmp>10 && tmp<20){
+            out = count+' '+many;
+        }else{
+            switch(count%10){
+                case 1: out = count+' '+first; break;
+                case 2: case 3: case 4: out = count+' '+some; break;
+                default: out = count+' '+many; break;
+            }
+        }
+        return out;
     };
 
 input.addEventListener('keydown',function(e){
@@ -19,12 +38,15 @@ input.addEventListener('keydown',function(e){
 });
 document.getElementById('sendButton').addEventListener('click',sendMessage);
 
-socket.on('connection',function(){
-    insertString('Присоединился новый участник');
+socket.on('connection',function(pid){
+    insertString('Присоединился новый участник','sysmsg',pid);
 });
-socket.on('disconnection',function(){
-    insertString('Один из участников отсоединился');
+socket.on('disconnection',function(pid){
+    insertString('Один из участников отсоединился','sysmsg',pid);
 });
 socket.on('message',function(data){
-    insertString(data);
+    insertString(data.text,'message',data.pid);
+});
+socket.on('count',function(data){
+    insertString('В чате '+someThing(data,'участник','участника','участников'),'important');
 });
